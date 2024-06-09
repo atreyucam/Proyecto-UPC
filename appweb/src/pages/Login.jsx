17eb1746ce88da3c9668808d3ae657c8d8d54d5e
AngelMelendres <fabian_melendres@hotmail.com>
2024-06-09 12:23:01 -0500

    inicio app web primera estreuctura para panel de administracion web

---
+++ appweb/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react";
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <Button>Button</Button>;
+    </>
+  );
+};
+
+export default Login;
