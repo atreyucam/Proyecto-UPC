32614bf7e332f551bd45b31c724851e44a80e5ae
Alex Camacho <camachoalex1k@gmail.com>
2024-08-23 22:34:17 -0500

    colors en home

--- app-web/src/pages/Home.jsx
+++ app-web/src/pages/Home.jsx
@@ -10,15 +10,34 @@ import {
   FiFlag,
   FiEye,
 } from "react-icons/fi";
+
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import EstadoBadge from "./components/EstadoBadge"; // Importa el componente
 
-const Button = ({ text, subText = "", number = null, onClick, icon }) => {
+// Función para determinar las clases de color en base al estado
+const getBadgeClass = (estado) => {
+  switch (estado) {
+    case "Pendiente":
+      return "bg-blue-100 text-blue-800 hover:bg-blue-200 hover:text-blue-900";
+    case "En progreso":
+      return "bg-yellow-100 text-yellow-800 hover:bg-yellow-200 hover:text-yellow-900";
+    case "Resuelto":
+      return "bg-green-100 text-green-800 hover:bg-green-200 hover:text-green-900";
+    case "Falso":
+      return "bg-red-100 text-red-800 hover:bg-red-200 hover:text-red-900";
+    default:
+      return "bg-gray-100 text-gray-800 hover:bg-gray-200 hover:text-gray-900";
+  }
+};
+
+const Button = ({ text, subText = "", number = null, onClick, icon, estado }) => {
+  const badgeClass = getBadgeClass(estado);
+
   return (
     <button
-      className="w-full h-full bg-white text-gray-800 p-4 rounded-lg shadow-md flex flex-col items-start justify-between hover:bg-black hover:text-white transition duration-300"
+      className={`w-full h-full ${badgeClass} p-4 rounded-lg shadow-md flex flex-col items-start justify-between transition duration-300`}
       onClick={onClick}
     >
       <div className="text-left flex justify-between items-center w-full">
@@ -41,7 +60,9 @@ Button.propTypes = {
   number: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   onClick: PropTypes.func.isRequired,
   icon: PropTypes.element.isRequired,
+  estado: PropTypes.string,
 };
+
 
 const Home = () => {
   const [stats, setStats] = useState({});
@@ -160,7 +181,7 @@ const Home = () => {
         <div className="p-5 bg-gray-100 rounded-lg">
           <Button
             text="Total Solicitudes"
-            subText={String(stats.total?.count || 0)}
+            number={stats.total?.count || 0}
             icon={<FiUserCheck size={24} />}
             onClick={() => console.log("Botón Total Solicitudes presionado")}
           />
@@ -168,9 +189,8 @@ const Home = () => {
         <div className="p-5 bg-gray-100 rounded-lg">
           <Button
             text="Solicitudes Resueltas"
-            subText={String(
-              stats.byStatus?.counts?.["Solicitudes resueltas"] || 0
-            )}
+            estado="Resuelto"
+            number={stats.byStatus?.counts?.["Solicitudes resueltas"] || 0}
             icon={<FiCheckCircle size={24} />}
             onClick={() =>
               console.log("Botón Solicitudes Resueltas presionado")
@@ -180,9 +200,8 @@ const Home = () => {
         <div className="p-5 bg-gray-100 rounded-lg">
           <Button
             text="Solicitudes Pendientes"
-            subText={String(
-              stats.byStatus?.counts?.["Solicitudes pendientes"] || 0
-            )}
+            estado="Pendiente"
+            number={stats.byStatus?.counts?.["Solicitudes pendientes"] || 0}
             icon={<FiSmile size={24} />}
             onClick={() =>
               console.log("Botón Solicitudes Pendientes presionado")
@@ -192,9 +211,8 @@ const Home = () => {
         <div className="p-5 bg-gray-100 rounded-lg">
           <Button
             text="Solicitudes en Progreso"
-            subText={String(
-              stats.byStatus?.counts?.["Solicitudes en Progreso"] || 0
-            )}
+            estado="En progreso"
+            number={stats.byStatus?.counts?.["Solicitudes en Progreso"] || 0}
             icon={<FiShield size={24} />}
             onClick={() =>
               console.log("Botón Solicitudes en Progreso presionado")
@@ -204,9 +222,8 @@ const Home = () => {
         <div className="p-5 bg-gray-100 rounded-lg">
           <Button
             text="Solicitudes Falsas"
-            subText={String(
-              stats.byStatus?.counts?.["Solicitudes falsas"] || 0
-            )}
+            estado="Falso"
+            number={stats.byStatus?.counts?.["Solicitudes falsas"] || 0}
             icon={<FiFlag size={24} />}
             onClick={() => console.log("Botón Solicitudes Falsas presionado")}
           />
@@ -235,7 +252,7 @@ const Home = () => {
       <div className="grid grid-cols-4 gap-4">
         <div className="col-span-3 bg-white p-4 rounded-lg shadow-md">
           <h2 className="text-lg font-bold mb-4">Desempeños</h2>
-         <ApexCharts
+          <ApexCharts
             options={barOptions}
             series={barSeries}
             type="bar"
@@ -260,7 +277,7 @@ const Home = () => {
 
             <div className="bg-white rounded-lg p-3 shadow-md">
               <div className="flex items-center">
-              <div className="mr-3">
+                <div className="mr-3">
                   <FiCheckCircle size={20} color="#28a745" />
                 </div>
                 <div>
@@ -292,9 +309,9 @@ const Home = () => {
                 <th className="py-2 px-4 text-left">Estado</th>
                 <th className="py-2 px-4 text-left">Subtipo</th>
                 <th className="py-2 px-4 text-left">Fecha</th>
-                <th className="border-b p-2 text-left">Distrito</th>
-                <th className="border-b p-2 text-left">Cantón</th>
-                <th className="border-b p-2 text-left">Subzona</th>
+                <th className="py-2 px-4 text-left">Distrito</th>
+                <th className="py-2 px-4 text-left">Cantón</th>
+                <th className="py-2 px-4 text-left">Subzona</th>
                 <th className="py-2 px-4 text-left">Acciones</th>
               </tr>
             </thead>
@@ -309,9 +326,9 @@ const Home = () => {
                   <td className="py-2 px-4">
                     {new Date(solicitud.fecha_creacion).toLocaleString()}
                   </td>
-                  <td className="py-2 px-4">{solicitud.ubicacion.distrito}</td>
-                  <td className="py-2 px-4">{solicitud.ubicacion.canton}</td>
-                  <td className="py-2 px-4">{solicitud.ubicacion.subzona}</td>
+                  <td className="py-2 px-4">{solicitud.ubicacion?.distrito || 'Sin Distrito'}</td>
+                  <td className="py-2 px-4">{solicitud.ubicacion?.canton || 'Sin Cantón'}</td>
+                  <td className="py-2 px-4">{solicitud.ubicacion?.subzona || 'Sin Subzona'}</td>
                   <td className="py-2 px-4 flex gap-2 justify-center">
                     <button
                       onClick={() => handleRowClick(solicitud)}
