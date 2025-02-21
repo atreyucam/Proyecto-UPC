cd28330d8e955627aa4d7f68008c376d2366ceaf
Alex Camacho <camachoalex1k@gmail.com>
2025-02-20 19:17:45 -0500

    modificacion para docker2

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,13 @@ const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {
       dialect: 'postgres',
       logging: console.log,
-      define: { timestamps: false }
+      define: { timestamps: false },
+      dialectOptions: {
+        ssl: {
+          require: true, // 🔴 Requerir SSL
+          rejectUnauthorized: false // 🔴 Evitar problemas con certificados autofirmados
+        }
+      }
     })
   : new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
       host: process.env.DB_HOST,
