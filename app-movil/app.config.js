66c21df4a905d9aaa3518bb406c0f90e85a32edd
Alex Camacho <camachoalex1k@gmail.com>
2025-07-05 14:36:50 -0500

    config ssl

--- app-movil/app.config.js
+++ app-movil/app.config.js
@@ -6,8 +6,15 @@ export default {
     slug: "Seguridad_Policial",
     version: "1.0.0",
     extra: {
-      API_URL: process.env.API_URL || "http://34.16.26.53:3000"
+      API_URL: process.env.API_URL || "http://34.16.26.53:3000",
+      eas: {
+        "projectId": "f197fca6-693c-43e4-b94a-10c8df1e92c2"
+      }
     },
+    android: {
+      package: "com.upc.upcdigital", // ← ¡AQUÍ!
+      versionCode: 1
+    }
     // ⏬ todo lo demás igual que en tu app.json
   }
 };
