7cf96586ef1dc62219a1270f03a8fd9b64442954
Alex Camacho <camachoalex1k@gmail.com>
2025-07-05 15:47:34 -0500

    cambios de movil

--- app-movil/app.config.js
+++ app-movil/app.config.js
@@ -6,13 +6,13 @@ export default {
     slug: "Seguridad_Policial",
     version: "1.0.0",
     extra: {
-      API_URL: process.env.API_URL || "http://34.16.26.53:3000",
+      API_URL: process.env.API_URL || "https://upc-test.duckdns.org/api",
       eas: {
         "projectId": "f197fca6-693c-43e4-b94a-10c8df1e92c2"
       }
     },
     android: {
-      package: "com.upc.upcdigital", // ← ¡AQUÍ!
+      package: "com.upc.upcdigital",
       versionCode: 1
     }
     // ⏬ todo lo demás igual que en tu app.json
